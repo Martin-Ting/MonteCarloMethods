80a83cd1723927e01de87e7dca5160819bed061b
Martin-Ting <martinhting@gmail.com>
2015-03-06 13:57:41 -0800

    Added Integration simulation

--- estimateXSquared.py
+++ estimateXSquared.py
@@ -2,40 +2,30 @@ import sys, random, math
 from random import *
 from math import sqrt
 
-MAXVAL=1000.0
-
 totalTrials = 0.0
 totalHits = 0.0
 currError = 0.0
 
-# a trial represents a single simulation
 def trial():
     global totalTrials
     global totalHits
-    x=randint(-MAXVAL, MAXVAL)/MAXVAL
-    y=randint(-MAXVAL, MAXVAL)/MAXVAL
 
-    totalTrials+=1.0
-
-    if sqrt(x*x + y*y) <= 1.0:
-        totalHits += 1.0
 
 def printResults():
-    currError = math.fabs((4.0*totalHits/totalTrials) - math.pi) / math.pi
+    ratio = totalHits/totalTrials
+    currError = math.fabs(ratio*100 - ideal) / ideal
     print('Results ===============================')
     print('Total Trials: ', totalTrials)
     print('Number of hits: ', totalHits)
-    print('Ratio: ', 4.0*totalHits/totalTrials)
-    print('Ideal: ', math.pi)
+    print('Ratio: ', ratio*100)
+    print('Ideal: ', ideal)
     print('Error: ', currError)
     print(' ======================================')
 
 def main(argv):
-    print("Welcome to estimating Pi");
-
+    print("Welcome to estimating the Integral of X**2 functions");
     totalTrials=0
     totalHits=0
-
     for i in range(10): # 10
         trial()
     print('10')
@@ -95,8 +85,6 @@ def main(argv):
         trial()
     print('10 000 000')
     printResults()
-
-    print('Note: Multiplication error may apply')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
